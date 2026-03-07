79aece3feb3cfcde2dc9271c78b814158902ca4a
PabloCdL <angelcoyoy07@gmail.com>
2026-03-07 17:10:43 -0600

    Creo el menu junto con do-while para salir cuando sea 5

--- Proyecto 1/Program.cs
+++ Proyecto 1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Concurrent;
+
+int opcion;
+do
+{
+    Console.WriteLine("1. Nuevo contenido");
+    Console.WriteLine("2. Reglas del sistema");
+    Console.WriteLine("3. Estadisticas de la sesión");
+    Console.WriteLine("4. Reinicar estadisticas");
+    Console.WriteLine("5. Salir");
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Caso1");
+            break;
+        case 2:
+            Console.WriteLine("Caso2");
+            break;
+        case 3:
+            Console.WriteLine("Caso3");
+            break;
+        case 4:
+            Console.WriteLine("Caso4");
+            break;
+        case 5:
+            Console.WriteLine("Salir");
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            break;
+    }
+}while (opcion != 5);
+Console.WriteLine("Resumen");
